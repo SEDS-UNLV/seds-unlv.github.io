59151a04a18fda0c46e84be86139be46d4df8603
Victor Taksheyev <victortaksheyev@gmail.com>
2020-01-05 13:12:59 -0800

    Updated Contact to Be Automatically Generated From JSON File

--- scripts/autoFill.js
+++ scripts/autoFill.js
@@ -41,7 +41,8 @@ var autofill =
         },
         "youtube": {
             "url": "https://www.youtube.com/channel/UCsZVNjRN5Dt3QDY0_PHx53A"
-        }
+        },
+        "address": "SEDS UNLV 4505 S. Maryland Pkwy MS 4027, Las Vegas NV 89154"
     }
 }
 
@@ -70,8 +71,32 @@ function populateDateLocation(jsonpath, id_name){
 }
 
 // Populating project_meetings
-populateDateLocation(autofill_json.meetings_and_events.project_meetings, "project_meetings");
+if (document.getElementById("project_meetings")){
+    populateDateLocation(autofill_json.meetings_and_events.project_meetings, "project_meetings");
+}
+
 // Populating general_meetings
-populateDateLocation(autofill_json.meetings_and_events.general_meetings, "general_meetings");
+if (document.getElementById("general_meetings")){
+    populateDateLocation(autofill_json.meetings_and_events.general_meetings, "general_meetings");
+}
+
 // Populating upcoming_launches
-populateDateLocation(autofill_json.meetings_and_events.upcoming_launches, "upcoming_launches    ");
+if (document.getElementById("upcoming_launches")){
+    populateDateLocation(autofill_json.meetings_and_events.upcoming_launches, "upcoming_launches");
+}
+
+//  ---------------------- Contact Page ----------------------
+
+var email_tag = document.getElementById('email_address');
+email_tag.href = "mailto:" + autofill_json.contact.email.url
+var text_node = document.createTextNode(autofill_json.contact.email.url)
+email_tag.appendChild(text_node)
+
+var ig_tag = document.getElementById('instagram_icon_link');
+ig_tag.href = autofill_json.contact.instagram.url
+
+var fb_tag = document.getElementById('facebook_icon_link');
+fb_tag.href = autofill_json.contact.facebook.url
+
+var yt_tag = document.getElementById('youtube_icon_link');
+yt_tag.href = autofill_json.contact.youtube.url
