a9538592f95fbf8e7c06071045245c5439a571ec
victortaksheyev <victortaksheyev@gmail.com>
2020-01-07 19:19:58 -0800

    update navigation

--- scripts/js.js
+++ scripts/js.js
@@ -17,6 +17,21 @@ function mobileChanges(){
 
 
 
+// Get the container element
+var btnContainer = document.getElementById("navbar-nav");
+
+// Get all buttons with class="btn" inside the container
+var links = btnContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
 /*
 
 
